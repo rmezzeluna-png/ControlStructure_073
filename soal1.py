c46d2e0ce491e4f90ce0fe553a5a0330464b79ca
rmezzeluna-png <rmezzeluna@gmail.com>
2026-09-22 10:19:17 +0700

    soal1 selesai

--- soal1.py
+++ soal1.py
@@ -1,4 +1,13 @@
 nilai = it(input("masukkan nilai : "))
 
-if nilai>=90:
+if nilai >= 90:
     print("exellent")
+elif nilai >= 80:
+    print("very good")
+elif nilai >= 70:
+    print("good")
+elif nilai >= 60:
+    print ("average")
+
+else: 
+    print("cupu")
